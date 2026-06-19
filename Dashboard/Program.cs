6e92fbd01e685074a160dde831146407d4645c2e
ManderO9 <hossem.chetoui@gmail.com>
2026-06-19 14:44:11 +0100

    moved the ideas to a separate page

--- Dashboard/Program.cs
+++ Dashboard/Program.cs
@@ -8,6 +8,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddLocalStorageServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<DataStore>();
+
 
 var app = builder.Build();
 
---
+++ Dashboard/Services/DataStore.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.JSInterop;
+using Supabase;
+
+namespace Dashboard;
+
+public class DataStore
+{
+    private readonly ILocalStorageService mLocalStorageService;
+
+    public DataStore(ILocalStorageService mLocalStorageService) => this.mLocalStorageService = mLocalStorageService;
+
+    public async Task<Client> GetClientAsync()
+    {
+        var appSettings = mLocalStorageService.GetAppSettings();
+        var options = new SupabaseOptions
+        {
+            AutoConnectRealtime = false
+        };
+
+        var client = new Client(appSettings.SupabaseUrl, appSettings.SupabaseKey, options);
+
+        await client.InitializeAsync();
+
+        return client;
+    }
+}
